f1dd622403cca367e729d98308c4ee72048cf201
Pungyeon <lasse@tengen.dk>
2017-05-01 17:19:11 +0200

    slowly adding to create-certificate module.

--- Modules/create-certificate.py
+++ Modules/create-certificate.py
@@ -1,7 +1,23 @@
-import logging
+from logging import *
 
 
 def create_cert():
     # change /etc/ssl/openssl.cnf
+
+    opensslconfig_location = "/etc/ssl/openssl.cnf"
+
+    try:
+        with open(opensslconfig_location) as sslconf:
+            sslconfiguration = sslconf.readlines()
+        log("INFO", "reading " + opensslconfig_location)
+        for index, line in enumerate(sslconfiguration):
+            if line.find("#req_extensions = v3_req # The extensions to add to a certificate request"):
+                sslconfiguration[index] = "req_extensions = v3_req # The extensions to add to a certificate request"
+            if line.find("[ v3_req ]"):
+                sslconfiguration[index + 1] = "subjectAltName = @alt_names"
+
+    except IOError:
+        log("ERROR", "Failed reading " + opensslconfig_location)
+
     print "OK"
     # create certs
